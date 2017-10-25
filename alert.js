7ebc8f99fb3e20dafd3f1f3603a107bd04ab58e6
shawnd <axe2781@gmail.com>
2017-10-25 11:04:14 -0400

    y

--- alert.js
+++ alert.js
@@ -1,10 +1,18 @@
 var textBox = document.getElementById("text-to-display");
-var printButton =document.getElementById("print button");
-var clearButton = document.getElementById("");
-var printArea =document.getElementById("print arear");
+var printButton =document.getElementById("print-button");
+var clearButton = document.getElementById("clear-button");
+var printArea =document.getElementById("printarea");
 
 printButton.addEventListener("click", function(){
     var text = text.value;
     printArea.innerText = printArea.innerText + text;
+});
+
+clearButton.addEventListener("click", function(){
+
+});
+
+clearButton.addEventListener("click", function(){
+    printArea.innerText = "";
 });
 
