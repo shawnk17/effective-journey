d3cafb2eca6bf7425641f7a57f4461bf257b1187
shawn <axe2781@gmail.com>
2017-10-25 22:52:20 -0400

    tip

--- tip.js
+++ tip.js
@@ -1,15 +1,14 @@
 var checkAmountText = document.getElementById("check-amount");
 var tipPercentagetext =document.getElementById("tip-percentage");
-var calculatorTipButton = document.getElementById("calculator-tip-button");
-var totalPayDiv = document.getElementById("total-pay-div")
+var calculatorTipButton = document.getElementById("calculator-tip");
+var totalPayDiv = document.getElementById("total-to-pay");
 
-calculatorTipButton.addEventListener("clicl", function(){
-    var checkAmount = parseFloat(checkAmount,value),
-        tipPercentagetext = parseFloat(tipPercentagetext.value),
-        tipAmount = parseFloat =(tipAmount.value),
-        totalPayDiv = parseFloat =(totalPayDiv.value),
+calculatorTipButton.addEventListener("click", function(){
+    var checkAmount = parseFloat(checkAmountText,value),
+        tipPercentage = parseFloat(tipPercentage.value)/100,
+        tipAmount = checkAmount * tipAmount.valu,
+        totalAmount = checkAmount + tipAmount.value,
         
-        totalPatDiv.innerHTML ="strong">CheckAmount
-
-})
+        totalPayDiv,innerHTML = totalAmount;
+});
 
